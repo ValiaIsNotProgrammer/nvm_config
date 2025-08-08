0333fde286111539cf6ff17486d8f95db948eed8
ValiaIsNotProgrammer <ValiaChernykh@yandex.ru>
2025-08-08 14:42:18 +0300

    fix(lsp): update pylsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,23 +6,46 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local util = require("lspconfig.util")
+
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    lspconfig.clangd.setup {}
+    local function find_local_pylsp()
+      local root = util.root_pattern(".venv", "pyproject.toml", "setup.py", ".git")(vim.fn.getcwd())
+      if not root then return nil end
+
+      local pylsp_path = root .. "/.venv/bin/python_lsp_server"
+      if vim.fn.executable(pylsp_path) == 1 then
+        return pylsp_path
+      end
+
+      return nil
+    end
+   
+
+    local local_pylsp = find_local_pylsp()
+    -- if not local_pylsp then
+    --   vim.notify("pylsp not found in local .venv. Falling back to global pylsp (if any)", vim.log.levels.WARN)
+    -- end
+
     lspconfig.pylsp.setup {
+      cmd = local_pylsp and { local_pylsp } or { "pylsp" },
+      root_dir = util.root_pattern("pyproject.toml", "setup.cfg", "setup.py", "requirements.txt", ".git"),
       settings = {
-        plugins = {
-          ruff = { enabled = true },
-          black = { enabled = true },
-          jedi_completion = { enabled = true },
-          jedi_definition = { enabled = true },
-          jedi_hover = { enabled = true },
-          jedi_references = { enabled = true },
-          jedi_signature_help = { enabled = true },
-          jedi_symbols = { enabled = true },
-          rope_autoimport = { enabled = true },
-          rope_completion = { enabled = true },
+        pylsp = {
+          plugins = {
+          mypy = {
+              enabled = true,
+              strict = true
+            },
+           pycodestyle = {
+              enabled = true,
+              maxLineLength = 120,
+            },
+            ruff = { enabled = true },
+            black = { enabled = true },
+          },
         },
       },
     }
