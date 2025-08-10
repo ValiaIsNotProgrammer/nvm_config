b0a800b03d26d1649cff8964aabfb43b89618734
ValiaIsNotProgrammer <ValiaChernykh@yandex.ru>
2025-08-10 18:09:51 +0300

    refactor(neo-tree): rm window-picker config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,28 +10,8 @@ return {
   lazy = false,
   cmd = "Neotree",
   config = function()
-    require("window-picker").setup {
-      autoselect_one = false,
-      include_current = false,
-      filter_rules = {
-        bo = {
-          filetype = { "neo-tree", "neo-tree-popup", "notify" },
-          buftype = { "terminal", "quickfix" },
-        },
-      },
-      other_win_hl_color = "#e35e4f",
-    }
-
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-    for type, icon in pairs({
-      Error = " ",
-      Warn = " ",
-      Info = " ",
-      Hint = ""
-    }) do
-      vim.fn.sign_define("DiagnosticSign" .. type, { text = icon, texthl = "DiagnosticSign" .. type })
-    end
 
     require("neo-tree").setup {
       window = {
